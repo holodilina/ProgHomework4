1ee12809f4063dd8c967ce2653e5b6532472585e
Natalya <holodilina.n@gmail.com>
2022-09-16 16:11:20 +0300

    adding

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+
+   int summ = 0;
+   int size = int.Parse(Console.ReadLine());
+   int[] mass = new int[size];
+
+   string s = Console.ReadLine();
+
+   for (int i = 0; i < s.Length; i++);
+   
+
+
