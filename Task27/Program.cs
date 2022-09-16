1614191318e443af68d06273cb27c1bf0df55100
Natalya <holodilina.n@gmail.com>
2022-09-16 16:17:51 +0300

    adding2

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,12 +2,22 @@
 Console.WriteLine("Введите число: ");
 
    int summ = 0;
+   int [i];
    int size = int.Parse(Console.ReadLine());
    int[] mass = new int[size];
 
+   Console.WriteLine("Задайте число массива: ");
    string s = Console.ReadLine();
 
    for (int i = 0; i < s.Length; i++);
-   
+   {
+    mass[i] = int.Parse(s[i].ToString());
+   }
+   for (int i = 0; i < mass.Length; i++);
+   {
+    summ = mass[i] + summ;
+   }
 
+Console.WriteLine(summ);
+ConsoleReadLine();
 
