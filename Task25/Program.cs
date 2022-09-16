8b27da46c94c85914cd1d17c5a19b05ea87e74dd
Natalya <holodilina.n@gmail.com>
2022-09-16 15:27:08 +0300

    изменения в Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+
+Console.WriteLine("Введите два числа: ");
+Console.Write("Введите первое число: ");
+var a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+var b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(power(a, b));
+
+static int power(int a, int b)
+{ 
+    var res = 1; 
+
+    if(b==0)
+        return res;
+
+    for (var i=1; i<=b; i++)
+    {
+        res *= a;
+    } 
+    return res;   
+}
