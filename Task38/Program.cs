ddd29a3bc282e45d4ed725e3235014215146c6c4
Natalya <holodilina.n@gmail.com>
2022-09-20 15:00:57 +0300

    добавление 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+var maxSize= 10000;
+
+Console.Write($"Введите размер массива от 1 до {maxSize}: ");
+string? sizeArrayStr = Console.ReadLine(); 
+
+if (string.IsNullOrWhiteSpace(sizeArrayStr)||
+    !int.TryParse(sizeArrayStr, out var sizeArray)
+    || sizeArray<1
+    || sizeArray>maxSize)
+{
+    Console.WriteLine("не верный ввод");
+    return;
+} 
+
+var summ=0;
+var array = CreateRandomArray(sizeArray);
+
+for(var i=0; i<sizeArray;i++)
+{
+        if(i%2!=0)
+            summ+=array[i];
+}
+
+Console.WriteLine($"Сумма: {summ}"); 
+
+static int [] CreateRandomArray(int size)
+{
+    var array = new int[size];
+    var rn = new Random();
+    for(var i=0; i < size; i++)
+    {
+        array[i]=rn.Next(-10, 11);
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
