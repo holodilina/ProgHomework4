8f4b76cd01b96b87b370bbab4e69ce81a6cc3a22
Natalya <holodilina.n@gmail.com>
2022-09-23 15:10:05 +0300

    Add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитать сколько чисел больше 0 ввел пользователь.
+
+Console.Write(":");
+
+string? input = Console.ReadLine();
+
+if(string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("sdfds");
+    return;
+} 
+var res = Count(input.Split(","));
+//var res = Count("0,7,8,-2,-2".Split(","));
+//var res = Count("1,-7,567,89,223".Split(","));
+Console.WriteLine($"результат {res}");
+
+int Count(string[] numbers)
+{
+    int res=0;
+    foreach(var n in numbers)
+    {
+        int p=0;
+        //Console.WriteLine(n.Trim());
+        if(int.TryParse(n.Trim(), out p)&&p>0)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+
+
+
